7a9cba4818430ea6fecdaffeda9f4da2a25d7208
yuqingzou <yuqingzou@users.noreply.github.com>
2017-07-05 10:49:43 -0400

    checking

--- autoencodermodule.py
+++ autoencodermodule.py
@@ -47,12 +47,12 @@ class autoencodermodle(object):
         examples_to_show = 10
         
         # Network Parameters
-        n_hidden_1 = self.unit_list[0] # 1st layer num features
-        n_hidden_2 = self.unit_list[1] # 2nd layer num features
+        n_hidden_1 = self.unit_list[0] # 1st layer num features, now initial as 10
+        n_hidden_2 = self.unit_list[1] # 2nd layer num features, now initial as 5
         n_input = 15 # Q15 data input (img shape: 15)
         
         
-        # tf Graph input (only pictures)
+        # tf data input 
         X = tf.placeholder("float", [None, n_input])
 
         weights = {
@@ -79,16 +79,6 @@ class autoencodermodle(object):
                                            biases['encoder_b2']))
             return layer_2
 
-
-        # Building the decoder
-        def decoder(x):
-            # Encoder Hidden layer with sigmoid activation #1
-            layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['decoder_h1']),
-                                           biases['decoder_b1']))
-            # Decoder Hidden layer with sigmoid activation #2
-            layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, weights['decoder_h2']),
-                                           biases['decoder_b2']))
-            return layer_2
 
         # Construct model
         encoder_op = encoder(X)
@@ -120,12 +110,7 @@ class autoencodermodle(object):
             # Run optimization op (backprop) and cost op (to get loss value)
                     _, c = sess.run([optimizer, cost], feed_dict={X: batch_xs})
                     s=s+n
-#==============================================================================
-#                     # Display logs per epoch step
-#                     if epoch % display_step == 0:
-#                         print("Epoch:", '%04d' % (epoch+1),
-#                               "cost=", "{:.9f}".format(c))
-#==============================================================================
+
 
             print("Optimization Finished!")
             self.hidden_1_weight = sess.run(weights['encoder_h1'])
@@ -133,7 +118,7 @@ class autoencodermodle(object):
             self.hidden_2_weight = sess.run(weights['encoder_h2'])
             self.hidden_2_biases = sess.run(biases['encoder_b1'])
             
-            
+  #### function for print the all weight and biases ####          
     def print_weight(self):
         """ print weight (variable object) for all layer"""
         print('first layer weight \n')
@@ -147,19 +132,4 @@ class autoencodermodle(object):
         print(self.hidden_1_biases)
         print('second layer biases\n')
         print(self.hidden_2_biases) 
-
-#==============================================================================
-#     # Applying encode and decode over test set
-#     encode_decode = sess.run(
-#         y_pred, feed_dict={X: mnist.test.images[:examples_to_show]})
-#     # Compare original images with their reconstructions
-#     f, a = plt.subplots(2, 10, figsize=(10, 2))
-#     for i in range(examples_to_show):
-#         a[0][i].imshow(np.reshape(mnist.test.images[i], (28, 28)))
-#         a[1][i].imshow(np.reshape(encode_decode[i], (28, 28)))
-#     f.show()
-#     plt.draw()
-#     plt.waitforbuttonpress()
-#==============================================================================
-
 
